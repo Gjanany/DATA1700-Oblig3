75c99c4c5ea85405ef6d65b0507faf068e13ffd8
Gjanany <gjananyk@gmail.com>
2024-05-02 17:28:45 +0200

    kinobillett.js

--- src/main/resources/static/kinobillett.js
+++ src/main/resources/static/kinobillett.js
@@ -1,52 +1,14 @@
-/*
-let kinoArray = [];
 
-function addToArray() {
-
-    console.log("array activated");
-    const Film = document.getElementById("film").value;
-    const Antall = document.getElementById("quantity").value;
-    const Fornavn = document.getElementById("firstname").value;
-    const Etternavn = document.getElementById("surname").value;
-    const Telefonnr = document.getElementById("phonenr").value;
-    const Epost = document.getElementById("email").value;
-
-
-    kinoArray.push({
-        FilmKey: Film, AntallKey: Antall, FornavnKey: Fornavn, EtternavnKey:
-        Etternavn, EpostKey: Epost, TelefonnrKey: Telefonnr
-    });
-    console.log(kinoArray);
-    populateHTML(kinoArray);
-}
-function populateHTML(kinoArray) {
-    console.log("kino")
-    let html = "<ol>";
-    console.log(kinoArray)
-    for (let i in kinoArray) {
-        console.log(kinoArray[i].FornavnKey);
-        html += "<li>" + kinoArray[i].FilmKey + " " + kinoArray[i].AntallKey + " " + kinoArray[i].FornavnKey
-            + " " + kinoArray[i].EtternavnKey + " " + kinoArray[i].EpostKey + " " + kinoArray[i].TelefonnrKey + "</li>";
-
-    }
-    html += "</ol>"
-    document.getElementById("result").innerHTML = html;
-    console.log(html);
-
-
- */
-
-function checkValid({data, felt}) {
-    // A couple of variables to avoid repetition
-    const felt_lc = felt.toLowerCase();
-    const error = "#" + felt_lc + "-error"
+function checkValid({data, field}) {
+    const field_LC = field.toLowerCase();
+    const error = "#" + field_LC + "-error"
 
     if (data === "") {
-        $(error).html(felt + " må fylles.");
+        $(error).html(field + " må fylles.");
         return false;
     }
-    if (data === ("--Velg " + felt_lc + "--")) {
-        $(error).html("Må velge " + felt_lc + ".");
+    if (data === ("--Velg " + field_LC + "--")) {
+        $(error).html("Må velges " + field_LC + ".");
         return false;
     }
 
@@ -54,7 +16,7 @@ function checkValid({data, felt}) {
     return true;
 
     /*Henter billetter*/
-    function hentAlle() {
+    function hentBilletter() {
         $.get("/hentAlle", function (billetter) {
             formatData(billetter);
         });
@@ -100,8 +62,8 @@ function checkValid({data, felt}) {
 
             };
             $.post("/lagre", billetter, function () {
-                hentAlle();
-                console.log("lagret billetter");
+                hentBilletter();
+                console.log("lagre billetter");
             });
             $("#film").val("-velg film");
             $("#quantity").val("");
@@ -118,79 +80,15 @@ function checkValid({data, felt}) {
     function slettEnBillett(id) {
         let url = "/slettEnBillett?id=" + id;
         $.get(url, function () {
-            hentAlle();
+            hentBilletter();
         });
     }
 
     function slettBilletter() {
         $.get("/slettBilletter", function () {
-            hentAlle();
+            hentBilletter();
         });
     }
 }
-/*
-    function deleteArray() {
-        kinoArray = [];
-        populateHTML(kinoArray);
-    }
 
-    function validateQuantity(quantity) {
-        const regexp = /^[0-9]{1,2}$/;
-        const valid = regexp.test(quantity);
-        if (!valid) {
-            $("#errorQuantity").html("skriv inn et tall mellom 1 og 100")
-            return false;
-        } else {
-            $("#errorQuantity").html("");
-            return true;
-        }
-    }
 
-    function validateFirstname(firstname) {
-        const regexp = /^[a-zA-ZæøåÆØÅ. \-]{1,30}$/;
-        const valid = regexp.test(firstname);
-        if (!valid) {
-            $("#errorFirstname").html("navn er påkrevd")
-            return false;
-        } else {
-            $("#errorFirstname").html("");
-            return true;
-        }
-    }
-
-    function validateSurname(surname) {
-        const regexp = /^[a-zA-ZæøåÆØÅ. \-]{1,30}$/;
-        const valid = regexp.test(surname);
-        if (!valid) {
-            $("#errorSurname").html("etternavn er påkrevd")
-            return false;
-        } else {
-            $("#errorSurname").html("");
-            return true;
-        }
-    }
-
-    function validateEmail(email) {
-        const regexp = /^[a-zA-Z0-9-_%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,50}$/;
-        const valid = regexp.test(email);
-        if (!valid) {
-            $("#errorEmail").html("e-post adresse med @ er påkrevd")
-            return false;
-        } else {
-            $("#errorEmail").html("");
-            return true;
-        }
-    }
-
-    function validatePhonenr(phonenr) {
-        const regexp = /^[0-9]{8}$/;
-        const valid = regexp.test(phonenr);
-        if (!valid) {
-            $("#errorPhonenr").html("tlfnr med 8 sifre er påkrevd")
-            return false;
-        } else {
-            $("#errorPhonenr").html("");
-            return true;
-        }
-}
- */
